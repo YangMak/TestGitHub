7beeb71eb624e48e33d8d368c96f80475c06ffd1
Mak <makyean@gmail.com>
2018-08-21 04:02:41 +0800

    Add SCSS & Auto-comment & vscode-icon

--- vscode-test/test.js
+++ vscode-test/test.js
@@ -1,1 +1,13 @@
 console.log('success File!!');
+
+export class Action {}
+
+function myFunction() {
+    console.log('Document This Test');
+    return true;
+}
+/** Auto comment Test
+ * @var
+ * @ array => adBlock 
+ */
+var adBlock = [];
